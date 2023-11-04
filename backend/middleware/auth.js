1c94148792e4c34132163485330984d799986d21
Hexdee <lukmansaheed79@gmail.com>
2023-11-04 18:18:12 +0100

    Remove transaction signing temporarily

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,18 +4,20 @@ const verifySignature = async (req, res, next) => {
   const signature =
     req.body.signature || req.query.signature
 
-  if (!signature) {
-    return res.status(403).send("A signature is required for authentication")
-  }
-  try {
-    // verify signature here
-    const message = 'Signing in to expresswage'
-    const recoveredAddress = recoverWalletAddress(signature, message)
-    req.user = recoveredAddress
+  // Because minipay does not support transaction signing yet
+    req.user = signature
+  // if (!signature) {
+  //   return res.status(403).send("A signature is required for authentication")
+  // }
+  // try {
+  //   // verify signature here
+  //   const message = 'Signing in to expresswage'
+  //   const recoveredAddress = recoverWalletAddress(signature, message)
+  //   req.user = recoveredAddress
 
-  } catch (err) {
-    return res.status(401).json({message: "Invalid signature"})
-  }
+  // } catch (err) {
+  //   return res.status(401).json({message: "Invalid signature"})
+  // }
   return next();
 };
 
