18f4010a6b090eeb856b91b3f06479190ecbdbf1
Lukman Saheed <lukmansaheed79@gmail.com>
2023-11-07 06:03:11 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const port = process.env.PORT || 4000 
 const app = express()
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
 app.get('/', async(req, res) => {
   res.send('Hello World!')
